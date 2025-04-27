0b4b0dff9fc54599bf16888e25880f6fb3daf5f6
iscid <iscivandelgado@gmail.com>
2025-04-27 11:47:10 -0600

    Se agregan datos a 01-objetos.js

--- JS-Moderno/Objetos/js/01-objetos.js
+++ JS-Moderno/Objetos/js/01-objetos.js
@@ -1,1 +1,25 @@
 console.log("Desde objetos");
+
+const nombre = "Monitor de 20 Pulgadas";
+const precio = 300;
+const disponible = true;
+
+const producto = {
+  nombre: "Monitor de 32 Pulgadas",
+  precio: 400,
+  disponible: false,
+};
+
+// Accede al objeto producto
+console.log(producto);
+
+// Accede a cada una de las propiedades
+console.log(`Precio: ${producto.precio}`);
+
+// Agregar propiedades al objeto
+producto.imagen = "imagen.jpg";
+console.log(producto);
+
+// Eliminar propiedades del objeto
+delete producto.disponible;
+console.log(producto);
