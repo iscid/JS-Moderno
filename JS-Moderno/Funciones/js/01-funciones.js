51a58f55c4158c6bb8c31c6fa15f06c041481c4c
iscid <iscivandelgado@gmail.com>
2025-05-01 16:02:37 -0600

    Se inicializa funciones/ con index.html y js/01-funciones.js

---
+++ JS-Moderno/Funciones/js/01-funciones.js
@@ -0,0 +1,14 @@
+// Declaración de una función
+function sumar() {
+  console.log(2 + 2);
+}
+
+// Llamada a la función
+sumar();
+
+// Expresión de función
+const restar = function () {
+  console.log(4 - 3);
+};
+
+restar();
