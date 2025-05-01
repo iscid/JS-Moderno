5773693cf4c482bc331001b1873f2559143afea6
iscid <iscivandelgado@gmail.com>
2025-05-01 16:29:04 -0600

    funciones nativas, parámetros y argumentos, parámetros por default

--- JS-Moderno/Funciones/js/01-funciones.js
+++ JS-Moderno/Funciones/js/01-funciones.js
@@ -12,3 +12,31 @@ const restar = function () {
 };
 
 restar();
+
+// Hoisting: permite utilizar las funciones que se definen de forma declarativa antes de ser creadas
+multiplicar();
+function multiplicar() {
+  console.log(5 * 4);
+}
+
+// Funciones nativas en JS
+// alert("Muestra una alerta"); Alerta en el navegador
+// prompt("¿Cuál es tu edad: ?"); Similar a la alerta, espera un valor
+// confirm("¿Deseas cerrar la ventana?"); Muestra una ventana con dos botones aceptar y cancelar
+// console.log("Muestra un mensaje en consola"); Info en consola
+
+// Funcion con parámetros
+function dividir(n1, n2) {
+  console.log(n1 / n2);
+}
+
+// Al momento de llamar la atención recibe argumentos
+dividir(10, 5);
+
+// Parámetros por defecto
+function saludar(nombre = "usuario", apellido = "x") {
+  console.log(`Hola: ${nombre} ${apellido}`);
+}
+
+saludar("Ivan", "Delgado");
+saludar();
